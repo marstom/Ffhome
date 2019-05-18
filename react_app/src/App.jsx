cdea33c25ce197c810a4698d5bfff9f9d5a11263
Tomasz Marszałek <marstomek@gmail.com>
2019-05-18 12:44:23 +0200

    Categories bars

--- react_app/src/App.jsx
+++ react_app/src/App.jsx
@@ -7,6 +7,7 @@ import { CostsTable } from './components/cost_table'
 import CostAdd from './components/cost_add'
 import Home from './components/home'
 import PlotComponent from './components/plot'
+import BarPlotComponent from './components/bar_plot'
 import {BrowserRouter as Router, Route, NavLink} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
@@ -52,6 +53,12 @@ function Plot() {
     return(
         <div><PlotComponent/></div>
     )
+}
+
+function BarPlot() {
+    return(
+        <div><BarPlotComponent/></div>
+    )
 
 }
 
@@ -64,6 +71,7 @@ function App() {
                     <NavLink className="link" to="/costs">Costs</NavLink>
                     <NavLink className="link" to="/categories">Categories</NavLink>
                     <NavLink className="link" to="/plot">Plot</NavLink>
+                    <NavLink className="link" to="/barplot">Bar Plot</NavLink>
                 </header>
 
 
@@ -71,6 +79,7 @@ function App() {
                 <Route path="/costs" component={CostTableView}/>
                 <Route path="/categories" component={CategoryAddView}/>
                 <Route path="/plot" component={Plot}/>
+                <Route path="/barplot" component={BarPlot}/>
             </Router>
         </div>
     );
